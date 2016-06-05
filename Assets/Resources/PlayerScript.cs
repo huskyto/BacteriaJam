3ccb3e0561c8cfbaf047b147495f23b9eb90c58a
Karl Tayfer <daPhyre.x@gmail.com>
2016-06-05 18:10:40 -0500

    + Bullets

--- Assets/Resources/PlayerScript.cs
+++ Assets/Resources/PlayerScript.cs
@@ -5,12 +5,15 @@ public class PlayerScript : MonoBehaviour {
 	private const int _moveSpeed = 3;
 	private const int _rotationSpeed = 360;
 	private float _angle = 0.0f;
+	private GameObject _bullet;
 	private Rigidbody2D _rigidbody2D;
 
 	// Use this for initialization
 	void Start () {
 		_rigidbody2D = GetComponent<Rigidbody2D> ();
 		_rigidbody2D.AddForce (new Vector2 (0, 100));
+
+		_bullet = Resources.Load<GameObject> ("Player Bullet/Player Bullet");
 	}
 	
 	// Update is called once per frame
@@ -31,6 +34,13 @@ public class PlayerScript : MonoBehaviour {
 
 		// Set speed
 		_rigidbody2D.velocity = new Vector2 (-Mathf.Sin(_angle * Mathf.Deg2Rad) * _moveSpeed, Mathf.Cos(_angle * Mathf.Deg2Rad) * _moveSpeed); //transform.up * _moveSpeed;
+
+		// Shoot
+		if (toggleShoot) {
+			GameObject b = Instantiate (_bullet);
+			b.transform.position = transform.position;
+			b.transform.rotation = Quaternion.AngleAxis (_angle, Vector3.forward);
+		}
 
 		// Camera follow
 		Vector3 pos = transform.position;
